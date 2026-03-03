3f4026fc2e01806582a4b47f5834da49d242ced2
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-03 15:30:55 +0900

    EmployeeSystem 완료

--- EmployeeSystem/Program.cs
+++ EmployeeSystem/Program.cs
@@ -1,4 +1,17 @@
 using System;
+using EmployeeSystem;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine($"=== 직원 급여 시스템 ===");
+Employee employee;
+
+employee = new Manager(1000000, "김부장", 5000000);
+employee.PrintInfo();
+Console.WriteLine();
+
+employee = new Developer("이개발", 4000000, 10);
+employee.PrintInfo();
+Console.WriteLine();
+
+employee = new Intern("박인턴");
+employee.PrintInfo();
+Console.WriteLine();
